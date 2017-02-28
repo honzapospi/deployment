86e7a0a7097f70dbf308ae00d1aedfa30e7c5300
Jan Pospisil <me@jan-pospisil.cz>
2017-02-28 13:43:54 +0100

    Fix error if deployment file not exist on remote host.

--- src/remote/create_deployment_file.js
+++ src/remote/create_deployment_file.js
@@ -2,23 +2,32 @@ const tracer = require('../tracer');
 const NO_SUCH_FILE = require('./error_messages').NO_SUCH_FILE;
 const PERMISSION_DENIED = require('./error_messages').PERMISSION_DENIED;
 
+function createFile(remoteList, deploymentFile, deployment, resolve, reject){
+    deployment.createFile(deploymentFile, JSON.stringify(remoteList)).then(() => {
+        resolve();
+    }).catch(e => {
+        if(e.message == NO_SUCH_FILE){
+            console.log(e.message);
+            processCreateFile(remoteList, deploymentFile, deployment, resolve, reject)
+            // tracer.error('Unable to create deployment file. Permission denied'+"\n");
+            // deployment.close();
+        } else if(e.message == PERMISSION_DENIED) {
+            console.log(e.message);
+            processCreateFile(remoteList, deploymentFile, deployment, resolve, reject)
+        } else {
+            throw e;
+        }
+    });
+}
+
 function processCreateFile(remoteList, deploymentFile, deployment, resolve, reject){
     deployment.delete(deploymentFile).then(() => {
-        deployment.createFile(deploymentFile, JSON.stringify(remoteList)).then(() => {
-            resolve();
-        }).catch(e => {
-            if(e.message == NO_SUCH_FILE){
-                console.log(e.message);
-                processCreateFile(remoteList, deploymentFile, deployment, resolve, reject)
-                // tracer.error('Unable to create deployment file. Permission denied'+"\n");
-                // deployment.close();
-            } else if(e.message == PERMISSION_DENIED) {
-                console.log(e.message);
-                processCreateFile(remoteList, deploymentFile, deployment, resolve, reject)
-            } else {
-                throw e;
-            }
-        });
+        createFile(remoteList, deploymentFile, deployment, resolve, reject);
+    }).catch(e => {
+        if(e.message == NO_SUCH_FILE){
+            createFile(remoteList, deploymentFile, deployment, resolve, reject);
+        } else
+            reject(e);
     })
 }
 
