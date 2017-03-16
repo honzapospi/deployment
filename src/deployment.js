6bc37a72f241b156a6556a353bd7128932c851d9
Jan Pospisil <me@jan-pospisil.cz>
2017-03-16 17:03:10 +0100

    Added test mode, which only print files to be upload and do not upload.

--- src/deployment.js
+++ src/deployment.js
@@ -22,27 +22,37 @@ function run(config, appRoot){
                 tracer.processStart('Creating deployment list of files.', true);
                 require('./create_lists')(appRoot + config.localRoot, deployedFiles, config.ignore).then((lists) => {
                     // upload files from local list
-                    tracer.printLine('Uploading files...');
-                    require('./remote/upload')(lists.localList, lists.remoteList, deployment, appRoot + config.localRoot, config.remoteRoot).then(uploaded => {
-                        // lists.remoteList should be + localList now
-                        tracer.printLine('Renaming files...');
-                        // rename uploaded files
-                        require('./remote/rename')(uploaded, config.remoteRoot, deployment).then(() => {
-                            // delete files
-                            tracer.printLine('Removing files...');
-                            require('./remote/delete')(lists.toRemove, config.remoteRoot, deployment).then(() => {
-                                // deploy deployment file
-                                require('./remote/create_deployment_file')(lists.remoteList, config.remoteRoot+config.deploymentFile, deployment).then(() => {
-                                    // purge
-                                    tracer.printLine('Purge...');
-                                    require('./remote/purge')(config.purge, deployment, config.remoteRoot).then(() => {
-                                        tracer.printLine('Deployment done.'+"\n");
-                                        deployment.close();
+                    // in test mode only display list of files to be uploaded
+                    if(config.test){
+                        console.log();
+                        lists.localList.map((item) => {
+                            console.log(item.name);
+                        })
+                        console.log('Total: '+lists.localList.length);
+                        deployment.close();
+                    } else {
+                        tracer.printLine('Uploading files...');
+                        require('./remote/upload')(lists.localList, lists.remoteList, deployment, appRoot + config.localRoot, config.remoteRoot).then(uploaded => {
+                            // lists.remoteList should be + localList now
+                            tracer.printLine('Renaming files...');
+                            // rename uploaded files
+                            require('./remote/rename')(uploaded, config.remoteRoot, deployment).then(() => {
+                                // delete files
+                                tracer.printLine('Removing files...');
+                                require('./remote/delete')(lists.toRemove, config.remoteRoot, deployment).then(() => {
+                                    // deploy deployment file
+                                    require('./remote/create_deployment_file')(lists.remoteList, config.remoteRoot+config.deploymentFile, deployment).then(() => {
+                                        // purge
+                                        tracer.printLine('Purge...');
+                                        require('./remote/purge')(config.purge, deployment, config.remoteRoot).then(() => {
+                                            tracer.printLine('Deployment done.'+"\n");
+                                            deployment.close();
+                                        });
                                     });
                                 });
                             });
                         });
-                    });
+                    }
                 });
             });
         })
